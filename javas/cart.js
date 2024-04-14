c395209d91e21887eb889591846f1339deea6480
Santiago Ismael López Contreras <74425838+SLC-ITESO@users.noreply.github.com>
2024-04-13 22:10:52 -0600

    Add files via upload

---
+++ javas/cart.js
@@ -0,0 +1,221 @@
+let cartArray = [];
+async function loadCartData(){
+    let user = sessionStorage.getItem('user')
+    if(user == null){
+        swal("Please log in order to cart","", "warning");
+    }
+    else{
+        let resp = await fetch('https://products-dasw.onrender.com/api/cart',{
+            method : 'GET',
+            headers:{
+                'x-expediente': '744857',
+                'x-user': user
+            }
+        })
+        //console.log(resp.status);
+        const data = await resp.json();
+        //console.log(data);
+        //console.log(data.cart);
+        sessionStorage.setItem('cart', JSON.stringify(data.cart))
+
+        cartArray = data;
+        showCartData(cartArray);
+    }
+}
+
+async function showCartData(cartArray){
+    let cart = JSON.parse(sessionStorage.getItem('cart'));
+    let html = /*html*/ `
+    <h1>Your Cart: ${cartArray.user}</h1>
+    <div class="row">
+        <div class="col-md-8">
+            <table class="table">
+                <thead>
+                <tr>
+                    <th style="width: 40%;">Product</th>
+                    <th style="width: 15%;">Price</th>
+                    <th style="width: 35%;">Quantity</th>
+                    <th style="width: 10%;"></th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody >
+    `;
+
+    html += cart.map((c) => /*html*/ `
+                <tr uuidRow="${c.product.uuid}">
+                    <td>
+                        <img src="${c.product.imageUrl}" alt="${c.product.name} Image" class="item-img">
+                        <label class="product-name">${c.product.name}</label>
+                    </td>
+                    <td>
+                        <label class="price-label">${c.product.pricePerUnit}</label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="number" class="form-control quantity-input" id="cartAmnt" value="${c.amount}" disabled>
+                        </label>
+                        <button class="btn btn-sm btn-outline-secondary edit-btn"><i class="fas fa-pencil-alt"></i></button>
+                        <button class="btn btn-sm btn-outline-danger cancel-btn" style="display: none;"><i class="bi bi-x-lg"></i></button>
+                        <button class="btn btn-sm btn-outline-success confirm-btn" onclick="updateAmount('${c.product.uuid}')" style="display: none;" 
+                        ><i class="fas fa-check"></i></button>
+                    </td>
+                    <td>
+                        <button class="btn btn-sm btn-outline-danger delete-icon-btn" onclick="deleteItem('${c.product.uuid}')"><i class="fas fa-trash-alt"></i></button>
+                    </td>
+                </tr>
+    `).join("");
+
+    html += /*html*/ `
+        </tbody>
+            </table>
+        </div>
+    `;
+    //checkout
+    html += /*html*/ `
+    <div class="col-md-4">
+            <table class="table">
+                <tbody>
+                <tr>
+                    <td>Total:</td>
+                    <td>${cartArray.total}</td>
+                </tr>
+                </tbody>
+            </table>
+            <div class="text-end">
+                <a href="#" class="btn btn-success">Checkout</a>
+            </div>
+        </div>
+    </div>
+</div>
+    `
+
+    document.querySelector('#cartTable').innerHTML = html;
+    addEditButtonList();
+}
+
+function addEditButtonList(){
+    const edButt = document.querySelectorAll('.edit-btn');
+    edButt.forEach(function(editButton){
+        editButton.addEventListener('click', function(){
+            const row = this.closest('tr'); // esto para que busque el tr mas cercano
+            // Muestra los botones de cancel y confirm y esconde el de edit
+            row.querySelector('.cancel-btn').style.display = 'inline-block';
+            row.querySelector('.confirm-btn').style.display = 'inline-block';
+            row.querySelector('.edit-btn').style.display = 'none';
+
+            // Habilita cantidad
+            const quantityInput = row.querySelector('.quantity-input');
+            quantityInput.removeAttribute('disabled');
+        })
+        const cancelButton = editButton.nextElementSibling;
+        cancelButton.addEventListener('click', function() {
+            const row = this.closest('tr');
+
+            // Cuando se preciona, vueve a su original
+            row.querySelector('.cancel-btn').style.display = 'none';
+            row.querySelector('.confirm-btn').style.display = 'none';
+            row.querySelector('.edit-btn').style.display = 'inline-block';
+
+            const quantityInput = row.querySelector('.quantity-input');
+            quantityInput.setAttribute('disabled', 'disabled');
+        });
+
+        //ahora lo mismo para confirmar
+        const confirmButton = editButton.nextElementSibling.nextElementSibling;
+        confirmButton.addEventListener('click', function() {
+            const row = this.closest('tr');
+
+            row.querySelector('.cancel-btn').style.display = 'none';
+            row.querySelector('.confirm-btn').style.display = 'none';
+            row.querySelector('.edit-btn').style.display = 'inline-block';
+
+            const quantityInput = row.querySelector('.quantity-input');
+            quantityInput.setAttribute('disabled', 'disabled');
+        });
+    })
+}
+
+function deleteItem(uuid){
+    let prods = JSON.parse(sessionStorage.getItem('products'));
+    let prodData = prods.find(p => p.uuid == uuid);
+    swal({
+        title: "Are you sure?",
+        text: prodData.name + " will be removed",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    })
+        .then(async (willDelete) => {
+            if (willDelete) {
+                await confirmDelete(prodData.uuid)
+            } else {
+                swal("The product is safe!");
+            }
+        });
+}
+
+async function confirmDelete(uuid){
+    link = 'https://products-dasw.onrender.com/api/cart/'+uuid;
+    let user = sessionStorage.getItem('user')
+    let resp = await fetch(link,{
+        method : 'DELETE',
+        headers:{
+            'x-expediente': '744857',
+            'x-user': user
+        }
+    }).then(
+        swal("Product deleted", "" , "success")
+    ).catch(err => {
+        swal("Error deleting product", err, "error");
+    })
+
+    loadCartData();
+}
+
+async function updateAmount(uuid){
+    //console.log('updating amount')
+    link = 'https://products-dasw.onrender.com/api/cart/'+uuid;
+    let user = sessionStorage.getItem('user')
+
+    let newAmountInput = document.querySelector(`tr[uuidRow="${uuid}"] .quantity-input`);
+    let newAmount = newAmountInput.value;
+    //console.log("NEW AMOUNT" + newAmount);
+    let amount = parseInt(newAmount);
+    //console.log("AMOUNT: "+amount);
+
+    //verificamos que no se pase de stock
+    let prods = JSON.parse(sessionStorage.getItem('products'));
+    //console.log(prods);
+    let prodData = prods.find(p => p.uuid == uuid);
+
+    if (amount == 0){
+        //console.log('confirm delete');
+        deleteItem(uuid);
+        return;
+    }
+    if(amount > prodData.stock || amount <= 0 || isNaN(amount)){
+        swal("Bad stock!", "", "error");
+        loadCartData();
+        throw new Error("Invalid amount");
+    }
+
+
+    let body = {amount}
+    let resp = await fetch(link,{
+        method : 'POST',
+        headers:{
+            'Content-type':'Application/json',
+            'x-expediente': '744857',
+            'x-user': user
+        },
+        body: JSON.stringify(body)
+
+    }).then(
+        swal("Product Updated", "" , "success"))
+    let data = await resp.json();
+    //console.log(data);
+
+    loadCartData();
+}
+
---
+++ javas/prods.js
@@ -0,0 +1,169 @@
+async function loadData(){
+    let resp = await fetch('https://products-dasw.onrender.com/api/products',{
+        method : 'GET',
+        headers:{
+            'x-expediente': '744857',
+            'x-auth': 'admin'
+        }
+    })
+    //console.log(resp.status);
+    let data = await resp.json();
+    //console.log(data);
+    sessionStorage.setItem('products', JSON.stringify(data))
+    sessionStorage.setItem('fullproducts', JSON.stringify(data))
+    updateCategories(data)
+    paginateData(data)
+    //showCardsData(data)
+
+}
+function paginateData(data){
+    let itemPerPage = 4;
+    let numPages = Math.ceil(data.length/itemPerPage);
+    let paginatedData = [];
+    for(let i = 0; i < numPages; i++){
+        paginatedData.push(data.slice(i*itemPerPage, (i+1)*itemPerPage));
+    }
+    let l = paginatedData.length;
+    //console.log(paginatedData);
+    sessionStorage.setItem('products', JSON.stringify(paginatedData.flat()))
+    addbuttons(l)
+    //showCardsData(paginatedData[0]);
+    showPagCardsData(1)
+}
+
+function addbuttons(l) {
+    //again, investigating more in stack overflow, I found the
+    // way of integrated a for in a string
+    let html = /*html*/ `
+    ${Array.from({length: l}, (_, i) => i).map((i) => /*html*/ `
+        <li class="page-item"><a class="page-link" onclick="showPagCardsData('${i+1}')">${i+1}</a></li>
+    `).join("")}`;
+    document.querySelector('#paginas').innerHTML = html;
+}
+
+function showPagCardsData(pageNum){
+    let prods = JSON.parse(sessionStorage.getItem('products'));
+    let itemPerPage = 4;
+    let paginatedData = prods.slice((pageNum-1)*itemPerPage, pageNum*itemPerPage);
+    showCardsData(paginatedData);
+
+}
+
+document.getElementById('searchButton').addEventListener('click', function() {
+    //console.log('searching');
+    let searchTerm = document.getElementById("seachInput").value;
+    //console.log(searchTerm);
+    if(searchTerm){
+        let prods = JSON.parse(sessionStorage.getItem('fullproducts'));
+        let filteredProds = prods.filter(prod => prod.name.toLowerCase().includes(searchTerm.toLowerCase()));
+        //console.log("FILTERED PRODS");
+        //console.log(filteredProds);
+        if(filteredProds.length == 0){
+            swal("No products found!", "", "error");
+            return;
+        }
+        paginateData(filteredProds);
+    }
+    else{
+        loadData()
+    }
+})
+
+function showCardsData(prodsArray) {
+    let html = /*html*/ `
+    <div id ="cardz" class="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4">
+    ${prodsArray
+        .map((prod) => /*html*/ `
+        <div class="col">
+            <div class="card h-100">
+                <img src="${prod.imageUrl}" class="card-img-top" alt="${prod.category}">
+                <div class="card-body">
+                    <h5 class="card-title">${prod.name}</h5>
+                    <p class="card-text">${prod.description}</p>
+                    
+                </div>
+                <div class="card-footer">
+                    <p class="card-text"><h2>$${prod.pricePerUnit}</h2></p>
+                    <a href="#" class="btn btn-primary" onclick="addCart('${prod.uuid}')">Add to Cart</a>
+                </div>
+            </div>
+        </div>
+    `).join("")}`;
+
+    document.querySelector('#product_html').innerHTML = html;
+}
+
+function updateCategories(data){
+    let categories = data.map(prod => prod.category);
+    //Investigando en stack overflow, se encontro la manera de eliminar
+    //los duplicados de un array, se utiliza el new Set y se convierte
+    //nuevamente a un array, los tres puntos son para
+    //que el array no se convierta en un array de arrays
+
+    categories = [...new Set(categories)];
+    //console.log("CATEGORIES")
+    //console.log(categories)
+    document.querySelector('#dropdownCat').innerHTML =
+        categories.map((c) => `
+    <li><a class="dropdown-item" onclick="asignaTipo('${c}')" href="categ.html">${c}</a></li>
+    <li><hr class="dropdown-divider"></li>
+    `).join("");
+}
+
+function asignaTipo(categoria){
+    //console.log(categoria);
+    //console.log(typeof(categoria));
+    sessionStorage.setItem('categoria', categoria);
+}
+
+function addCart(uuid){
+    //console.log("ENTROOOOO")
+    if(sessionStorage.getItem('user') == null){
+        swal("You must be logged in to add products to the cart", "", "warning");
+        return;
+    }
+    let prods = JSON.parse(sessionStorage.getItem('products'));
+    let prodData = prods.find(p => p.uuid == uuid);
+    //console.log(prodData);
+
+    document.getElementById('modalItemImage').src = prodData.imageUrl;
+    document.getElementById('modalItemName').textContent = prodData.name;
+    document.getElementById('modalItemPrice').textContent = '$' + prodData.pricePerUnit;
+    document.getElementById('addCartButton').setAttribute('onclick', `confirmAdd('${prodData.uuid}')`);
+    // Show the modal
+    let modal = new bootstrap.Modal(document.getElementById('preCartModal'), {});
+    modal.show();
+}
+
+async function confirmAdd(uuid){
+    let prods = JSON.parse(sessionStorage.getItem('products'));
+    let prodData = prods.find(p => p.uuid == uuid);
+
+    amount = parseInt(document.getElementById('quantity').value);
+    //console.log(amount);
+    if(amount > prodData.stock || amount <= 0 || isNaN(amount)){
+        swal("Bad stock!", "", "error");
+        return;
+    }
+    let user = sessionStorage.getItem('user');
+    let link = 'https://products-dasw.onrender.com/api/cart/'+prodData.uuid;
+
+    let body = {amount};
+
+    let resp = await fetch(link,{
+        method : 'POST',
+        headers:{
+            'x-expediente': '744857',
+            'x-user': user,
+            'Content-type':'Application/json'
+        },
+        body: JSON.stringify(body)
+
+    }).catch(err => {
+        swal("Error adding product to cart", err, "error");
+        return;
+    });
+    let data = await resp.json();
+    //console.log(data);
+    swal("Product added to cart", "", "success");
+}
